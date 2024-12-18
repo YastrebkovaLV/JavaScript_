4f2606ea1573b68d0e66d4ff05a283b64fd78b48
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-18 18:07:59 +0300

    Условия

--- task1.js
+++ task1.js
@@ -1,74 +1,110 @@
+// Условия
+
 // Задание 1
-
-const MyName = "Lilia";
-const ProgrammingLanquage = "JavaScript";
-const ReasonText = " мне интересен язык ";
-const NumberOfTime = "10 секунд,ахаха";
-
-console.log(` Меня зовут, ${MyName}. Сейчас я изучаю язык программирования ${ProgrammingLanquage} на паре по ${ProgrammingLanquage}. Я хочу стать разработчиком , потому что ${ReasonText}. До этого я изучала ${ProgrammingLanquage} ${NumberOfTime} .`);
+let number = 5;
+if (number % 2 === 0) {
+    console.log("Четное число");
+} else {
+    console.log("Нечетное число");
+}
 
 // Задание 2
-
-const Text = "Я учу джаваскрипт";
-
-console.log(Text.length);
-console.log(Text.charAt(0) + Text.charAt(8));
+let time = 22;
+if (time >= 5 && time < 12) {
+    console.log("Утро");
+} else if (time >= 12 && time < 18) {
+    console.log("День");
+} else if (time >= 18 && time < 22) {
+    console.log("Вечер");
+} else {
+    console.log("Ночь");
+}
 
 // Задание 3
-
-const InfoText = ` Меня зовут, ${MyName}. Сейчас я изучаю язык программирования ${ProgrammingLanquage.toUpperCase()}
-` +
-    ` на паре по ${ProgrammingLanquage.toUpperCase()} . 
-` +
-    `Я хочу стать разработчиком, потому что ${ReasonText}. 
-` +
-    `До этого я изучала ${ProgrammingLanquage.toUpperCase()} 
-` +
-    `времени ${NumberOfTime}.`
-
-
-
-const First = InfoText.charAt(1);
-const Last = InfoText.charAt(212);
-
-console.log(InfoText.length -1);
-console.log(InfoText);
-console.log(First + Last );
+let age = 20;
+if (age >= 18) {
+    console.log("Вы совершеннолетний");
+} else {
+    console.log("Вы несовершеннолетний");
+}
 
 // Задание 4
-
-let text = prompt("Как вас зовут?");
-
-text = text.toLowerCase().trim();
+let num1 = 40;
+let num2 = 555;
+if (num1 > num2) {
+    console.log("Наибольшее число: " + num1);
+} else if (num2 > num1) {
+    console.log("Наибольшее число: " + num2);
+} else {
+    console.log("Числа равны");
+}
 
 // Задание 5
-
-let Age = prompt(" Сколько вам лет? ");
-
-Age = Number(Age);
-
-alert(` Вас зовут : ${text} и вам ${Age} лет`);
+let grade = 55;
+if (grade >= 90) {
+    console.log("Отлично");
+} else if (grade >= 75) {
+    console.log("Хорошо");
+} else if (grade >= 60) {
+    console.log("Удовлетворительно");
+} else {
+    console.log("Неудовлетворительно");
+}
 
 // Задание 6
-
-let userString = prompt("Введите текст для обрезки").trim();
-
-let startSliceIndex = parseInt(prompt("Введите индекс, с которого нужно начать обрезку строки"));
-
-let endSliceIndex = parseInt(prompt("Введите индекс, которым нужно закончить обрезку строки"));
-
-let resultString = userString.slice(startSliceIndex, endSliceIndex);
-
-alert("Результат: " + resultString);
+let price = 22;
+let money = 1110;
+if (money >= price) {
+    console.log("Покупка возможна");
+} else {
+    console.log("Недостаточно средств");
+}
 
 // Задание 7
+let day = 3;
+if (day >= 1 && day <= 5) {
+    console.log("Рабочий день");
+} else {
+    console.log("Выходной день");
+}
 
-let userText = prompt("Введите текст").trim();
+// Задание 8
+let str = "Привет, мир!";
+if (str.length > 10) {
+    console.log("Длинная строка");
+} else {
+    console.log("Короткая строка");
+}
 
-let wordFromText = prompt("Введите слово из текста").trim();
+// Задание 9
+let inputPassword = "12345";
+let correctPassword = "12345";
+if (inputPassword === correctPassword) {
+    console.log("Доступ разрешен");
+} else {
+    console.log("Неверный пароль");
+}
 
-let indexOfWord = userText.indexOf(wordFromText);
+// Задание 10
+let Hour = 5;
+if (Hour >= 0 && Hour < 6) {
+    console.log("Спать");
+} else if (Hour >= 6 && Hour < 12) {
+    console.log("Работать");
+} else if (Hour >= 12 && Hour < 14) {
+    console.log("Обедать");
+} else {
+    console.log("Работать");
+}
 
-let trimmedText = userText.substring(0, indexOfWord);
-
-alert("Результат: " + trimmedText);
+// Задание 11
+let month = 6;
+if (month === 12 || month <= 2) {
+    console.log("Зима");
+} else if (month >= 3 && month <= 5) {
+    console.log("Весна");
+} else if (month >= 6 && month <= 8) {
+    console.log("Лето");
+} else {
+    console.log("Осень");
+}

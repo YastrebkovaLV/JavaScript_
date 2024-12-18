7ef53277246f2d37e3b9d44bf989f3ee8e939750
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-18 17:31:06 +0300

    Переменные

---
+++ index.js
@@ -0,0 +1,23 @@
+// Переменные
+
+// Задание 1: Изменение значение переменной
+
+let a = 12;
+console.log(a);
+
+a = 555;
+console.log(a);
+
+// Задание 2: Использование "const"
+
+const b = 7;
+console.log(b); // const - неизменяемый вид переменной
+
+// Задание 3: Вывод переменных
+
+const name = "Lilia";
+console.log(name);
+let age = 18;
+console.log(age);
+
+

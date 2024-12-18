04441aa45b706de5ae121d53bd04c3e18205a5ae
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-18 17:49:13 +0300

    Строки

--- task1.js
+++ task1.js
@@ -1,51 +1,74 @@
-// Типы данных
+// Задание 1
 
-const Name = "Lilia";
-console.log(typeof Name);
+const MyName = "Lilia";
+const ProgrammingLanquage = "JavaScript";
+const ReasonText = " мне интересен язык ";
+const NumberOfTime = "10 секунд,ахаха";
 
-const Age = 18;
-console.log(typeof Age);
-
-const A = true;
-console.log(typeof A);
-
-let B
-console.log(typeof B);
-
-const c = null;
-console.log(typeof c);
-
-const MyId = Symbol('id');
-console.log(typeof MyId);
-
-const FirstNumber = BigInt(42);
-console.log(typeof FirstNumber);
-
-const child = {
-    Nick: 'Lilia',
-    Age: 18,
-
-}
-
-console.log(typeof child);
+console.log(` Меня зовут, ${MyName}. Сейчас я изучаю язык программирования ${ProgrammingLanquage} на паре по ${ProgrammingLanquage}. Я хочу стать разработчиком , потому что ${ReasonText}. До этого я изучала ${ProgrammingLanquage} ${NumberOfTime} .`);
 
 // Задание 2
 
-let number = 33;
-let string = "lol";
-let boolean = true;
+const Text = "Я учу джаваскрипт";
 
-console.log("Изначальные значения:");
-console.log("number:", number);
-console.log("stringVar:", string);
-console.log("booleanVar:", boolean);
+console.log(Text.length);
+console.log(Text.charAt(0) + Text.charAt(8));
 
-number = "Теперь я строка";
-string = 100;
-boolean = null;
+// Задание 3
 
-console.log("Измененные значения:");
-console.log("numberVar:", number);
-console.log("stringVar:", string);
-console.log("booleanVar:", boolean);
+const InfoText = ` Меня зовут, ${MyName}. Сейчас я изучаю язык программирования ${ProgrammingLanquage.toUpperCase()}
+` +
+    ` на паре по ${ProgrammingLanquage.toUpperCase()} . 
+` +
+    `Я хочу стать разработчиком, потому что ${ReasonText}. 
+` +
+    `До этого я изучала ${ProgrammingLanquage.toUpperCase()} 
+` +
+    `времени ${NumberOfTime}.`
 
+
+
+const First = InfoText.charAt(1);
+const Last = InfoText.charAt(212);
+
+console.log(InfoText.length -1);
+console.log(InfoText);
+console.log(First + Last );
+
+// Задание 4
+
+let text = prompt("Как вас зовут?");
+
+text = text.toLowerCase().trim();
+
+// Задание 5
+
+let Age = prompt(" Сколько вам лет? ");
+
+Age = Number(Age);
+
+alert(` Вас зовут : ${text} и вам ${Age} лет`);
+
+// Задание 6
+
+let userString = prompt("Введите текст для обрезки").trim();
+
+let startSliceIndex = parseInt(prompt("Введите индекс, с которого нужно начать обрезку строки"));
+
+let endSliceIndex = parseInt(prompt("Введите индекс, которым нужно закончить обрезку строки"));
+
+let resultString = userString.slice(startSliceIndex, endSliceIndex);
+
+alert("Результат: " + resultString);
+
+// Задание 7
+
+let userText = prompt("Введите текст").trim();
+
+let wordFromText = prompt("Введите слово из текста").trim();
+
+let indexOfWord = userText.indexOf(wordFromText);
+
+let trimmedText = userText.substring(0, indexOfWord);
+
+alert("Результат: " + trimmedText);

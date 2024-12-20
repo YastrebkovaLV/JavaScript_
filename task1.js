802950c0249665c2333523fc4248be9faaf77a9a
Lilia Yastrebkova <kekovichl436@gmail.com>
2024-12-20 12:47:37 +0300

    Массивы

--- task1.js
+++ task1.js
@@ -1,83 +1,48 @@
-// Циклы
+// Массивы
 
-// Задание 1
+// 1. Создайте массив, содержащий числа от 1 до 5, и выведите его на экран.
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
 
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
+// 2. Создайте массив с именами ["John", "Ivan", "Peter"] и выведите имя "Peter" на экран.
+let names = ["John", "Ivan", "Peter"];
+console.log(names[2]); // Peter
+
+// 3. В массиве ["Apple", "Banana", "Orange"] замените "Banana" на "Mango" и выведите результат.
+let fruits = ["Apple", "Banana", "Orange"];
+fruits[1] = "Mango"; // Заменяем "Banana" на "Mango"
+console.log(fruits);
+
+// 4. В массив ["Dog", "Cat"] добавьте элемент "Hamster" в конец массива и выведите результат.
+let pets = ["Dog", "Cat"];
+pets.push("Hamster"); // Добавляем "Hamster"
+console.log(pets);
+
+// 5. В массиве ["Red", "Green", "Blue"] удалите первый элемент и выведите результат.
+let colors = ["Red", "Green", "Blue"];
+colors.shift(); // Удаляем первый элемент
+console.log(colors);
+
+// 6. Создайте массив чисел от 5 до 15. Пройдитесь по массиву циклом и выведите каждый элемент на экран.
+let range = [];
+for (let i = 5; i <= 15; i++) {
+    range.push(i);
+}
+for (let num of range) {
+    console.log(num);
 }
 
-// Задание 2
-
-for (let i = 10; i >= 1; i--) {
-    console.log(i);
-}
-
-// Задание 3
-
-const arr = [2, 4, 6, 8, 10];
+// 7. В массиве чисел [20, 20, 30, 40, 50] найдите сумму всех элементов и выведите результат.
+let numbersArray = [20, 20, 30, 40, 50];
 let sum = 0;
-for (let num of arr) {
+for (let num of numbersArray) {
     sum += num;
 }
 console.log(sum);
 
-// Задание 4
+// 8. В массиве чисел [23, 45, 67, 12, 89, 54, 37, 120] найдите максимальное число и выведите его.
+let anotherArray = [23, 45, 67, 12, 89, 54, 37, 120];
+let maxNumber = Math.max(...anotherArray);
+console.log(maxNumber);
 
-const array = [2, 4, 6, 8, 10];
-console.log(array.length);
-
-// Задание 5
-
-const fruits = ['apple', 'banana', 'orange'];
-console.log(fruits.includes('banana'));
-
-// Задание 6
-
-const user = {
-    name: 'John',
-    age: 30,
-    city: 'New York'
-};
-for (let key in user) {
-    console.log(key + ': ' + user[key]);
-}
-
-// Задание 7
-
-const array1 = [3, 7, 2, 9, 1];
-let min = array1[0];
-for (let num of array1) {
-    if (num < min) {
-        min = num;
-    }
-}
-console.log(min);
-
-// Задание 8
-
-const array2 = [5, 12, 18, 3, 20, 7];
-let count = 0;
-for (let num of array2) {
-    if (num > 10) {
-        count++;
-    }
-}
-console.log(count);
-
-// Задание 9
-
-let i = 1;
-while (i <= 10) {
-    console.log(i);
-    i++;
-}
-
-// Задание 10
-
-const str = 'JavaScript';
-let reversedStr = '';
-for (let i = str.length - 1; i >= 0; i--) {
-    reversedStr += str[i];
-}
-console.log(reversedStr);
 
